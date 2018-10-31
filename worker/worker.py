fe8e0a9a58b699b0878f93f7d2d1db68650a2386
Pierre-Emmanuel <pe@360boost.com>
2018-10-31 17:04:24 +0100

    reformat file

--- worker/worker.py
+++ worker/worker.py
@@ -4,18 +4,22 @@ import click
 import os
 import json
 
+
 async def fetch(session, url):
     async with session.get(url) as response:
         return await response.text()
 
+
 async def main():
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'http://python.org')
+        html = await fetch(session, "http://python.org")
         print(html)
+
 
 @click.group()
 def cli():
     pass
+
 
 @click.command()
 def launch():
@@ -24,21 +28,23 @@ def launch():
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
 
+
 @click.command()
-@click.option('--rijk-key',
-            prompt="What is your Rijksmuseum API key(https://www.rijksmuseum.nl/en/api)",
-            hide_input=True)
+@click.option(
+    "--rijk-key",
+    prompt="What is your Rijksmuseum API key(https://www.rijksmuseum.nl/en/api)",
+    hide_input=True,
+)
 def config(rijk_key):
     """ """
-    passfile = open('.artkeys', 'w')
+    passfile = open(".artkeys", "w")
 
-    data = {
-        "rijkkey" : rijk_key
-    }
+    data = {"rijkkey": rijk_key}
 
     passfile.write(json.dumps(data))
     passfile.close()
     click.echo("Config ok")
+
 
 cli.add_command(launch)
 cli.add_command(config)
